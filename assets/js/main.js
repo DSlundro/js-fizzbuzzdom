fa627ea3a2202ea230b0d4fd2a41e2b061a0080d
DSlundro <dragan.savic95@outlook.it>
2022-03-28 17:04:18 +0200

    script in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,23 @@ for ( i = 1; i <= 100; i++){
     // multipli di 3 e di 5
     if(i % 15 == 0){
         console.log('FizzBuzz');
+        myBox.innerHTML += `<div class="my_box_${i} bg-danger">fizzibuzz</div>`
     } 
     // multipli di 5
     else if (i % 5 == 0){
-        console.log('Buzz')
+        console.log('Buzz');
+        myBox.innerHTML += `<div class="my_box_${i} bg-warning">buzz</div>`
     } 
     // multipli di 3
     else if (i % 3 == 0 ){
-        console.log("Fizz")
+        console.log("Fizz");
+        myBox.innerHTML += `<div class="my_box_${i} bg-success">fizzi</div>`
     } 
     // numeri da 1 a 100
     else {
-        console.log(i)
+        console.log(i);
+        myBox.innerHTML += `<div class="my_box_${i} bg-primary">${i}</div>`
     }
-
 
 }
 
